2ecda0446c6b1be66e91edadff649ab1de6ad775
joaopedro1422 <jpcros40414@gmail.com>
2023-09-19 00:23:07 -0300

    psoft

--- src/main/java/com/ufcg/psoft/commerce/model/Entregador/Entregador.java
+++ src/main/java/com/ufcg/psoft/commerce/model/Entregador/Entregador.java
@@ -1,6 +1,7 @@
 package com.ufcg.psoft.commerce.model.Entregador;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ufcg.psoft.commerce.enums.TipoVeiculo;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,20 +12,39 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name= "entregadores")
 public class Entregador {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @JsonProperty("id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,name = "pk_id_entregador")
     private Long id;
 
 
     @JsonProperty("codigoAcesso")
+    @Column(nullable = false, name = "codigoAcesso_entregador")
     private String codigoAcesso;
 
     @JsonProperty("nome")
+    @Column(nullable = false, name = "nome_entregador")
     private String nome;
+
+    @JsonProperty("placaVeiculo")
+    @Column(nullable = false, name = "placaVeiculo_entregador")
+    private String placaVeiculo;
+
+    @JsonProperty("tipoVeiculo")
+    @Column(nullable = false, name = "tipoVeiculo_entregador")
+    private TipoVeiculo tipoVeiculo;
+
+    @JsonProperty("corVeiculo")
+    @Column(nullable = false, name = "corVeiculo_entregador")
+    private String corVeiculo;
+
+    @JsonProperty("aprovado")
+    @Column(nullable = false, name = "desc_aprovaçap_entregador")
+    private boolean aprovado;
 
 
     public Long getId() {
@@ -41,5 +61,45 @@ public class Entregador {
 
     public void setCodigoAcesso(String codigoAcesso) {
         this.codigoAcesso = codigoAcesso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPlacaVeiculo() {
+        return placaVeiculo;
+    }
+
+    public void setPlacaVeiculo(String placaVeiculo) {
+        this.placaVeiculo = placaVeiculo;
+    }
+
+    public TipoVeiculo getTipoVeiculo() {
+        return tipoVeiculo;
+    }
+
+    public void setTipoVeiculo(TipoVeiculo tipoVeiculo) {
+        this.tipoVeiculo = tipoVeiculo;
+    }
+
+    public String getCorVeiculo() {
+        return corVeiculo;
+    }
+
+    public void setCorVeiculo(String corVeiculo) {
+        this.corVeiculo = corVeiculo;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
     }
 }
